56dde951f61d96484a10d8b21be99f7a69d68683
ntnx0246 <shawngazin@protonmail.com>
2023-01-08 19:02:18 -0800

    created drivetrain and utils folder

---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+public class DriveTrain {
+    
+    private TalonFX frontRight;
+    private TalonFX backRight;
+    private TalonFX frontLeft;
+    private TalonFX backLeft;
+
+    
+}
