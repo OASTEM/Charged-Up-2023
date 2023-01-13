708639cb2e275c6b9d3e5ca9a052234baad547f7
FRC Member <>
2023-01-13 15:40:54 -0800

    limelight added

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -5,16 +5,33 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 
 public class Limelight extends SubsystemBase {
+  private NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+  private NetworkTableEntry tx; 
+  private NetworkTableEntry ty; 
+  private NetworkTableEntry ta;
+  private NetworkTableEntry tv;
+  private double x;
+  private double y;
+  
   /** Creates a new Limelight. */
   public Limelight() {
-    
+    this.tx = table.getEntry("tx");
+    this.ty = table.getEntry("ty");
+    this.ta = table.getEntry("ta");
+    this.tv = table.getEntry("tv");
+
   }
 
   @Override
   public void periodic() {
+    this.x = tx.getDouble(0.0);
+    this.y = ty.getDouble(0.0);
     // This method will be called once per scheduler run
   }
 }
