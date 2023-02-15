2be25b0454d9bf88316d990b15f0e083507e4d15
FRC Member <>
2023-02-14 17:07:46 -0800

    uncommented software code

--- src/main/java/frc/robot/commands/Calibration/CalibrationSequence.java
+++ src/main/java/frc/robot/commands/Calibration/CalibrationSequence.java
@@ -33,8 +33,8 @@ public class CalibrationSequence extends ParallelCommandGroup {
         new CalibratePivot(arm),
         new InstantCommand(arm::resetEncoders),
         new InstantCommand(arm::resetSideEncoders)
-        //new InstantCommand(arm::ArmSoftLimit),
-        //new InstantCommand(arm::setArmSoftLimit)
+        new InstantCommand(arm::ArmSoftLimit),
+        new InstantCommand(arm::setArmSoftLimit)
       )
     );
   }
